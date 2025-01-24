0239eed0b3a97449343a9b9686ce4d45b41af983
backspace <anh.phan156@protonmail.com>
2025-01-24 17:39:20 -0500

    keymaps

--- config/myConfig/lua/keymaps/general/init.lua
+++ config/myConfig/lua/keymaps/general/init.lua
@@ -5,27 +5,27 @@ vim.keymap.set("n", "Y", "<ESC>Vy", { silent = true, desc = "Copy line" })
 
 -- Quote wrapping keymaps
 -- Quotes
-vim.keymap.set("v", "<leader><leader>'", "<ESC>`>a'<ESC>`<i'<ESC>", { silent = true, desc = "wrap single quote" })
-vim.keymap.set("v", "<leader><leader>\"", "<ESC>`>a\"<ESC>`<i\"<ESC>", { silent = true, desc = "wrap double quote" })
+vim.keymap.set("v", "<leader>'", "<ESC>`>a'<ESC>`<i'<ESC>", { silent = true, desc = "wrap single quote" })
+vim.keymap.set("v", "<leader>\"", "<ESC>`>a\"<ESC>`<i\"<ESC>", { silent = true, desc = "wrap double quote" })
 
 -- Escape quotes
-vim.keymap.set("v", "<leader><leader>e'", "<ESC>`>a\\'<ESC>`<i\\'<ESC>",
+vim.keymap.set("v", "<leader><leader>'", "<ESC>`>a\\'<ESC>`<i\\'<ESC>",
   { silent = true, desc = "wrap escape single quote" })
-vim.keymap.set("v", "<leader><leader>e\"", "<ESC>`>a\\\"<ESC>`<i\\\"<ESC>",
+vim.keymap.set("v", "<leader><leader>\"", "<ESC>`>a\\\"<ESC>`<i\\\"<ESC>",
   { silent = true, desc = "wrap escape double quote" })
 
 -- Brackets
-vim.keymap.set("v", "<leader><leader>(", "<ESC>`>a)<ESC>`<i(<ESC>", { silent = true, desc = "wrap parenthese" })
-vim.keymap.set("v", "<leader><leader>[", "<ESC>`>a]<ESC>`<i[<ESC>", { silent = true, desc = "wrap square backet" })
-vim.keymap.set("v", "<leader><leader>{", "<ESC>`>a}<ESC>`<i{<ESC>", { silent = true, desc = "wrap curly backet" })
+vim.keymap.set("v", "<leader>(", "<ESC>`>a)<ESC>`<i(<ESC>", { silent = true, desc = "wrap parenthese" })
+vim.keymap.set("v", "<leader>[", "<ESC>`>a]<ESC>`<i[<ESC>", { silent = true, desc = "wrap square backet" })
+vim.keymap.set("v", "<leader>{", "<ESC>`>a}<ESC>`<i{<ESC>", { silent = true, desc = "wrap curly backet" })
 
 -- Escape brackets
-vim.keymap.set("v", "<leader><leader>e(", "<ESC>`>a\\)<ESC>`<i\\(<ESC>",
+vim.keymap.set("v", "<leader><leader>(", "<ESC>`>a\\)<ESC>`<i\\(<ESC>",
   { silent = true, desc = "wrap escape parenthese" })
 
 -- Others
-vim.keymap.set("v", "<leader><leader>`", "<ESC>`>a`<ESC>`<i`<ESC>", { silent = true, desc = "wrap backtick" })
-vim.keymap.set("v", "<leader><leader><", "<ESC>`>a><ESC>`<i<<ESC>", { silent = true, desc = "wrap triangle" })
+vim.keymap.set("v", "<leader>`", "<ESC>`>a`<ESC>`<i`<ESC>", { silent = true, desc = "wrap backtick" })
+vim.keymap.set("v", "<leader><", "<ESC>`>a><ESC>`<i<<ESC>", { silent = true, desc = "wrap triangle" })
 
 -- Tmux
 vim.keymap.set("n", "<leader>cs", "<CMD>! tmux send-keys -t :.+1 C-c<CR>",
