bbd295852688aa02f62121e4c9e026bc8c9c1416
backspace <anh.phan156@protonmail.com>
2026-01-07 12:47:24 -0500

    treesitter config

--- config/myConfig/lua/general/treesitter/init.lua
+++ config/myConfig/lua/general/treesitter/init.lua
@@ -4,11 +4,23 @@ local parsers = {
 	'rust',
 	'bash',
 	'nix',
+	'git_config',
+	'git_rebase',
+	'gitcommit',
+	'gitignore',
+	'diff',
+	'make',
+	'json',
+	'regex',
 }
-require('nvim-treesitter').setup()
+
+require 'nvim-treesitter'.setup {
+	-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+	install_dir = vim.fn.stdpath('data') .. '/site'
+}
 
 vim.defer_fn(function()
-	require('nvim-treesitter').install(parsers):wait(300000)
+	require('nvim-treesitter').install(parsers)
 end, 0)
 
 vim.api.nvim_create_autocmd('FileType', {
